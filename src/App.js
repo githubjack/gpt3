f13eb7f7985859cb7e15fd468fbeb4281b710a80
Jack <busada777@gmail.com>
2022-01-16 21:52:31 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react'
+
+/*
+This import statement will be placed inside of containers
+----------------------------------------------------------
+import { Article, Brand, CTA, Feature, Navbar } from './components';
+to structure better our application
+
+*/
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+           <div className="gradient__bg">
+               <Navbar />
+               <Header />
+           </div>
+
+           <Brand />
+           <WhatGPT3 />
+           <Features />
+           <Possibility />
+           <CTA />
+           <Blog />
+           <Footer />
+
+        </div>
+    );
 }
 
 export default App;
